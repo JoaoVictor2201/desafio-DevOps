12d34efd689f55c706d77a73b81f66ebd282865a
JoaoVictor2201 <franca.jv@hotmail.com>
2025-11-20 00:17:20 -0300

    novos teste e troca de nome das rotinas

--- test_app.py
+++ test_app.py
@@ -2,29 +2,29 @@ import unittest
 from app import app
 import werkzeug
 
-# Patch temporário para adicionar o atributo '__version__' em werkzeug 
 if not hasattr(werkzeug, '__version__'): 
     werkzeug.__version__ = "mock-version"
 
 class APITestCase(unittest.TestCase): 
     @classmethod 
-    def setUpClass(cls): 
-        # Criação do cliente de teste 
+    def setUpClass(cls):
         cls.client = app.test_client()
-    
-    def test_home(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, {"message": "API is running"})
 
-    def test_login(self):
-        response = self.client.post('/login')
+    def test_get_items(self):
+        response = self.client.get('/items')
         self.assertEqual(response.status_code, 200)
-        self.assertIn('access_token', response.json)
+        self.assertIn('items', response.json)
+        self.assertEqual(response.json['items'], ["item1", "item2", "item3"])
 
-    def test_protected_no_token(self):
-        response = self.client.get('/protected')
-        self.assertEqual(response.status_code, 401)
+    def test_is_return_list(self):
+        response = self.client.get('/items')
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json['items'], list)
+
+    def test_swagger_ui_accessible(self):
+        response = self.client.get('/items')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'item1', response.data)
 
 if __name__ == '__main__':
     unittest.main()
